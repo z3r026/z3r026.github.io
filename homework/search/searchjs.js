ce58b93719009b3421e0418815e783c6c56dd596
z3r026 <alb10005@byui.edu>
2016-12-05 10:36:31 -0700

    URL
    link probs

--- homework/search/searchjs.js
+++ homework/search/searchjs.js
@@ -6,7 +6,7 @@ $('#query').keyup(function(){
 
     var rExp = new RegExp(value, "i");
 
-$.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+$.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
   console.log(data);
   }); // end getJSON
 
@@ -14,14 +14,14 @@ $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", fu
 $('#query').keyup(function() {
   var value = $('#query').val();
   var rExp = new RegExp(value, "i");
-  $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
+  $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
     console.log(data); // test for JSON received
     // Begin building output
     var output = '<ol>';
     $.each(data.RESULTS, function(key, val) {
       if (val.name.search(rExp) != -1) {
         output += '<li>';
-        output += '<a href="http://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '<a href="https://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
         output += '</li>';
       }
     }); // end each
