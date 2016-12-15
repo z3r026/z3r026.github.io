1aa302a0f20fe9c1267bf6277fef092e40a1cdd6
z3r026 <alb10005@byui.edu>
2016-12-14 22:35:33 -0700

    updates
    last

---
+++ final-proj/modules/modules.js
@@ -0,0 +1,6 @@
+
+$(function () {
+  $("#page-header").load("/final-project/modules/header.html");
+  $("nav").load("/final-project/modules/nav.html");
+  $("footer").load("/final-project/modules/footer.html");
+});
